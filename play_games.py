bdc13793853ca926312dd4c0836bc581aea6947b
Kevin <kevin@soloitguy.com>
2018-03-17 20:35:16 -0500

    Added mac functionality

--- play_games.py
+++ play_games.py
@@ -1,7 +1,7 @@
 import os
 import platform
 platformOS = platform.system()
-
+options = ["Play Games", "Write Code", "Browse the Internet"]
 if (platformOS == "Windows"):
     for retry in range(5):
         activity = raw_input("Do you want to play a game? ")
@@ -16,8 +16,55 @@ if (platformOS == "Windows"):
         print "Must answer 'yes' or 'no'."
     else:
         print "5 invalid entries.  Exiting"
+elif (platformOS == "Darwin"):
+    
+    def let_user_pick(options):
+        print "What would you like to do today?"
+        for idx, element in enumerate(options):
+            print"{}) {}".format(idx+1,element)
+        i = input("Enter number: ")
+        try:
+            if (0 < int(i) <= len(options)):
+                return int(i)
+        except:
+            pass
+        return None
+
+    choice = let_user_pick( options)
+    
+    if (choice == 1):
+        file_to_open = "/Applications/Spotify.app"
+        import subprocess
+        subprocess.call(["open", "-a", file_to_open])
+    
+    if (choice == 2):
+        file_to_open = "/Applications/Atom.app"
+        import subprocess
+        subprocess.call(["open", "-a", file_to_open]) 
+        print "Happy Coding!" 
+    
+    if (choice == 3):
+        file_to_open = "/Applications/Google Chrome.app"
+        import subprocess
+        subprocess.call(["open", "-a", file_to_open]) 
+        print "Happy Webbing!"       
+    
+    else:
+        print "Not ready yet."        
+    
+    
 else:
-    print "Only Windows is Supported."
+    print "Only Windows and MacOS are Supported."
 
-
+# def let_user_pick(options):
+#     print("Please choose:")
+#     for idx, element in enumerate(options):
+#         print("{}) {}".format(idx+1,element))
+#     i = input("Enter number: ")
+#     try:
+#         if 0 < int(i) <= len(options):
+#             return int(i)
+#     except:
+#         pass
+#     return None
   
